fe510f04c47019f2887acafeaf6b94e33442799f
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-03-05 16:24:03 -0600

    workflowr::wflow_publish("smallsim.Rmd")

--- scripts/temp.R
+++ scripts/temp.R
@@ -1,9 +1,9 @@
 library(fastTopics)
 library(cowplot)
 load("../output/nips/fits-nips.RData")
-i <- which(dat$k == 11)
+i <- which(dat$k == 12)
 plot_progress(fits[i],x = "iter",add.point.every = 100) +
   theme_cowplot(font_size = 10)
-fit1 <- poisson2multinom(fits[["fit-nips-em-k=11"]])
-fit2 <- poisson2multinom(fits[["fit-nips-scd-ex-k=11"]])
+fit1 <- poisson2multinom(fits[["fit-nips-em-k=12"]])
+fit2 <- poisson2multinom(fits[["fit-nips-scd-ex-k=12"]])
 plot(fit1$L,fit2$L,pch = 20)
