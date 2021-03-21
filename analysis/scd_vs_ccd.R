e4a30e7ab04cdbdbdbe7ce363041136d2ac81f0d
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-03-21 11:41:43 -0500

    Added SCD with extrapolation to scd_vs_ccd.R example.

--- analysis/scd_vs_ccd.R
+++ analysis/scd_vs_ccd.R
@@ -14,28 +14,37 @@ writeMat("../matlab/dat100x200.mat",X = X,L0 = fit0$L,F0 = fit0$F)
 fit3 <- readMat("../matlab/ccd100x200.mat")
 fit3$obj <- drop(fit3$obj)
 
-# Run 20 multiplicative (EM) updates.
+# Run 40 multiplicative (EM) updates.
 fit1 <- suppressWarnings(nnmf(X,k = 3,init = list(W = fit0$L,H = t(fit0$F)),
                               method = "lee",loss = "mkl",max.iter = 40,
                               trace = 1,rel.tol = 0,inner.max.iter = 1,
                               inner.rel.tol = 0,n.threads = 1))
 
-# Run 20 SCD updates.
+# Run 40 SCD updates.
 fit2 <- suppressWarnings(nnmf(X,k = 3,init = list(W = fit0$L,H = t(fit0$F)),
                               method = "scd",loss = "mkl",max.iter = 40,
                               trace = 1,rel.tol = 0,inner.max.iter = 1,
                               n.threads = 1))
 
+# Run 40 extrapolated updates.
+fit4 <- fit_poisson_nmf(X,fit0 = fit0,numiter = 40,method = "scd",
+                        control = list(extrapolate = TRUE,numiter = 1))
+
 print(range(fit2$W - fit3$W))
 print(range(fit2$H - fit3$H))
 
-y0 <- min(c(fit1$mkl,fit2$mkl,fit3$obj))
-y1 <- diff(range(c(fit1$mkl,fit2$mkl,fit3$obj)))
+fit4$loss <- (sum(X*log(X + 1e-15) - X)
+              - (fit4$progress[21:60,"loglik"]
+                 - sum(fastTopics:::loglik_poisson_const(X))))/(100*200)
+
+y0 <- min(c(fit1$mkl,fit2$mkl,fit3$obj,fit4$loss))
+y1 <- diff(range(c(fit1$mkl,fit2$mkl,fit3$obj,fit4$loss)))
 iter <- 1:40
 plot(iter,fit1$mkl - y0,type = "l",col = "darkblue",lwd = 2,
      xlab = "iteration",ylab = "loss",ylim = c(0,y1))
 points(iter,fit1$mkl - y0,col = "darkblue",pch = 20,cex = 1)
 lines(iter,fit2$mkl - y0,col = "darkorange",lwd = 2)
 points(iter,fit2$mkl - y0,col = "darkorange",pch = 20,cex = 1)
-lines(iter,fit3$obj - y0,col = "red",lwd = 1)
-
+lines(iter,fit3$obj - y0,col = "dodgerblue",lwd = 1)
+lines(iter,fit4$loss - y0,col = "magenta",lwd = 2)
+points(iter,fit4$loss - y0,col = "magenta",pch = 20,cex = 1)
