bedc090a4eee17f4f3c51d571c4b22f3a517e399
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-04-07 08:58:29 -0500

    Added droplet and 68k pbmc fits to fit_all_models.sh.

--- scripts/fit_all_models.sh
+++ scripts/fit_all_models.sh
@@ -68,20 +68,14 @@ sbatch ${SCRIPT_FIT} nips  2 em     1000  no fit-nips-em-k=2
 sbatch ${SCRIPT_FIT} nips  2 scd    1000  no fit-nips-scd-k=2
 sbatch ${SCRIPT_FIT} nips  2 em     1000 yes fit-nips-em-ex-k=2
 sbatch ${SCRIPT_FIT} nips  2 scd    1000 yes fit-nips-scd-ex-k=2
-
-#                    data  k method    n  ex outfile
 sbatch ${SCRIPT_FIT} nips  3 em     1000  no fit-nips-em-k=3
 sbatch ${SCRIPT_FIT} nips  3 scd    1000  no fit-nips-scd-k=3
 sbatch ${SCRIPT_FIT} nips  3 em     1000 yes fit-nips-em-ex-k=3
 sbatch ${SCRIPT_FIT} nips  3 scd    1000 yes fit-nips-scd-ex-k=3
-
-#                    data  k method    n  ex outfile
 sbatch ${SCRIPT_FIT} nips  4 em     1000  no fit-nips-em-k=4
 sbatch ${SCRIPT_FIT} nips  4 scd    1000  no fit-nips-scd-k=4
 sbatch ${SCRIPT_FIT} nips  4 em     1000 yes fit-nips-em-ex-k=4
 sbatch ${SCRIPT_FIT} nips  4 scd    1000 yes fit-nips-scd-ex-k=4
-
-#                    data  k method    n  ex outfile
 sbatch ${SCRIPT_FIT} nips  5 em     1000  no fit-nips-em-k=5
 sbatch ${SCRIPT_FIT} nips  5 scd    1000  no fit-nips-scd-k=5
 sbatch ${SCRIPT_FIT} nips  5 em     1000 yes fit-nips-em-ex-k=5
@@ -92,20 +86,14 @@ sbatch ${SCRIPT_FIT} nips  6 em     1000  no fit-nips-em-k=6
 sbatch ${SCRIPT_FIT} nips  6 scd    1000  no fit-nips-scd-k=6
 sbatch ${SCRIPT_FIT} nips  6 em     1000 yes fit-nips-em-ex-k=6
 sbatch ${SCRIPT_FIT} nips  6 scd    1000 yes fit-nips-scd-ex-k=6
-
-#                    data  k method    n  ex outfile
 sbatch ${SCRIPT_FIT} nips  7 em     1000  no fit-nips-em-k=7
 sbatch ${SCRIPT_FIT} nips  7 scd    1000  no fit-nips-scd-k=7
 sbatch ${SCRIPT_FIT} nips  7 em     1000 yes fit-nips-em-ex-k=7
 sbatch ${SCRIPT_FIT} nips  7 scd    1000 yes fit-nips-scd-ex-k=7
-
-#                    data  k method    n  ex outfile
 sbatch ${SCRIPT_FIT} nips  8 em     1000  no fit-nips-em-k=8
 sbatch ${SCRIPT_FIT} nips  8 scd    1000  no fit-nips-scd-k=8
 sbatch ${SCRIPT_FIT} nips  8 em     1000 yes fit-nips-em-ex-k=8
 sbatch ${SCRIPT_FIT} nips  8 scd    1000 yes fit-nips-scd-ex-k=8
-
-#                    data  k method    n  ex outfile
 sbatch ${SCRIPT_FIT} nips  9 em     1000  no fit-nips-em-k=9
 sbatch ${SCRIPT_FIT} nips  9 scd    1000  no fit-nips-scd-k=9
 sbatch ${SCRIPT_FIT} nips  9 em     1000 yes fit-nips-em-ex-k=9
@@ -116,14 +104,10 @@ sbatch ${SCRIPT_FIT} nips 10 em     1000  no fit-nips-em-k=10
 sbatch ${SCRIPT_FIT} nips 10 scd    1000  no fit-nips-scd-k=10
 sbatch ${SCRIPT_FIT} nips 10 em     1000 yes fit-nips-em-ex-k=10
 sbatch ${SCRIPT_FIT} nips 10 scd    1000 yes fit-nips-scd-ex-k=10
-
-#                    data  k method    n  ex outfile
 sbatch ${SCRIPT_FIT} nips 11 em     1000  no fit-nips-em-k=11
 sbatch ${SCRIPT_FIT} nips 11 scd    1000  no fit-nips-scd-k=11
 sbatch ${SCRIPT_FIT} nips 11 em     1000 yes fit-nips-em-ex-k=11
 sbatch ${SCRIPT_FIT} nips 11 scd    1000 yes fit-nips-scd-ex-k=11
-
-#                    data  k method    n  ex outfile
 sbatch ${SCRIPT_FIT} nips 12 em     1000  no fit-nips-em-k=12
 sbatch ${SCRIPT_FIT} nips 12 scd    1000  no fit-nips-scd-k=12
 sbatch ${SCRIPT_FIT} nips 12 em     1000 yes fit-nips-em-ex-k=12
@@ -135,20 +119,14 @@ sbatch ${SCRIPT_FIT} newsgroups  2 em     1000  no fit-newsgroups-em-k=2
 sbatch ${SCRIPT_FIT} newsgroups  2 scd    1000  no fit-newsgroups-scd-k=2
 sbatch ${SCRIPT_FIT} newsgroups  2 em     1000 yes fit-newsgroups-em-ex-k=2
 sbatch ${SCRIPT_FIT} newsgroups  2 scd    1000 yes fit-newsgroups-scd-ex-k=2
-
-#                    data        k method    n  ex outfile
 sbatch ${SCRIPT_FIT} newsgroups  3 em     1000  no fit-newsgroups-em-k=3
 sbatch ${SCRIPT_FIT} newsgroups  3 scd    1000  no fit-newsgroups-scd-k=3
 sbatch ${SCRIPT_FIT} newsgroups  3 em     1000 yes fit-newsgroups-em-ex-k=3
 sbatch ${SCRIPT_FIT} newsgroups  3 scd    1000 yes fit-newsgroups-scd-ex-k=3
-
-#                    data        k method    n  ex outfile
 sbatch ${SCRIPT_FIT} newsgroups  4 em     1000  no fit-newsgroups-em-k=4
 sbatch ${SCRIPT_FIT} newsgroups  4 scd    1000  no fit-newsgroups-scd-k=4
 sbatch ${SCRIPT_FIT} newsgroups  4 em     1000 yes fit-newsgroups-em-ex-k=4
 sbatch ${SCRIPT_FIT} newsgroups  4 scd    1000 yes fit-newsgroups-scd-ex-k=4
-
-#                    data        k method    n  ex outfile
 sbatch ${SCRIPT_FIT} newsgroups  5 em     1000  no fit-newsgroups-em-k=5
 sbatch ${SCRIPT_FIT} newsgroups  5 scd    1000  no fit-newsgroups-scd-k=5
 sbatch ${SCRIPT_FIT} newsgroups  5 em     1000 yes fit-newsgroups-em-ex-k=5
@@ -159,20 +137,14 @@ sbatch ${SCRIPT_FIT} newsgroups  6 em     1000  no fit-newsgroups-em-k=6
 sbatch ${SCRIPT_FIT} newsgroups  6 scd    1000  no fit-newsgroups-scd-k=6
 sbatch ${SCRIPT_FIT} newsgroups  6 em     1000 yes fit-newsgroups-em-ex-k=6
 sbatch ${SCRIPT_FIT} newsgroups  6 scd    1000 yes fit-newsgroups-scd-ex-k=6
-
-#                    data        k method    n  ex outfile
 sbatch ${SCRIPT_FIT} newsgroups  7 em     1000  no fit-newsgroups-em-k=7
 sbatch ${SCRIPT_FIT} newsgroups  7 scd    1000  no fit-newsgroups-scd-k=7
 sbatch ${SCRIPT_FIT} newsgroups  7 em     1000 yes fit-newsgroups-em-ex-k=7
 sbatch ${SCRIPT_FIT} newsgroups  7 scd    1000 yes fit-newsgroups-scd-ex-k=7
-
-#                    data        k method    n  ex outfile
 sbatch ${SCRIPT_FIT} newsgroups  8 em     1000  no fit-newsgroups-em-k=8
 sbatch ${SCRIPT_FIT} newsgroups  8 scd    1000  no fit-newsgroups-scd-k=8
 sbatch ${SCRIPT_FIT} newsgroups  8 em     1000 yes fit-newsgroups-em-ex-k=8
 sbatch ${SCRIPT_FIT} newsgroups  8 scd    1000 yes fit-newsgroups-scd-ex-k=8
-
-#                    data        k method    n  ex outfile
 sbatch ${SCRIPT_FIT} newsgroups  9 em     1000  no fit-newsgroups-em-k=9
 sbatch ${SCRIPT_FIT} newsgroups  9 scd    1000  no fit-newsgroups-scd-k=9
 sbatch ${SCRIPT_FIT} newsgroups  9 em     1000 yes fit-newsgroups-em-ex-k=9
@@ -183,15 +155,113 @@ sbatch ${SCRIPT_FIT} newsgroups 10 em     1000  no fit-newsgroups-em-k=10
 sbatch ${SCRIPT_FIT} newsgroups 10 scd    1000  no fit-newsgroups-scd-k=10
 sbatch ${SCRIPT_FIT} newsgroups 10 em     1000 yes fit-newsgroups-em-ex-k=10
 sbatch ${SCRIPT_FIT} newsgroups 10 scd    1000 yes fit-newsgroups-scd-ex-k=10
-
-#                    data        k method    n  ex outfile
 sbatch ${SCRIPT_FIT} newsgroups 11 em     1000  no fit-newsgroups-em-k=11
 sbatch ${SCRIPT_FIT} newsgroups 11 scd    1000  no fit-newsgroups-scd-k=11
 sbatch ${SCRIPT_FIT} newsgroups 11 em     1000 yes fit-newsgroups-em-ex-k=11
 sbatch ${SCRIPT_FIT} newsgroups 11 scd    1000 yes fit-newsgroups-scd-ex-k=11
-
-#                    data        k method    n  ex outfile
 sbatch ${SCRIPT_FIT} newsgroups 12 em     1000  no fit-newsgroups-em-k=12
 sbatch ${SCRIPT_FIT} newsgroups 12 scd    1000  no fit-newsgroups-scd-k=12
 sbatch ${SCRIPT_FIT} newsgroups 12 em     1000 yes fit-newsgroups-em-ex-k=12
 sbatch ${SCRIPT_FIT} newsgroups 12 scd    1000 yes fit-newsgroups-scd-ex-k=12
+
+# Fit factorizations to droplet data, with and without extrapolation.
+#                    data     k method    n  ex outfile
+sbatch ${SCRIPT_FIT} droplet  2 em     1000  no fit-droplet-em-k=2
+sbatch ${SCRIPT_FIT} droplet  2 scd    1000  no fit-droplet-scd-k=2
+sbatch ${SCRIPT_FIT} droplet  2 em     1000 yes fit-droplet-em-ex-k=2
+sbatch ${SCRIPT_FIT} droplet  2 scd    1000 yes fit-droplet-scd-ex-k=2
+sbatch ${SCRIPT_FIT} droplet  3 em     1000  no fit-droplet-em-k=3
+sbatch ${SCRIPT_FIT} droplet  3 scd    1000  no fit-droplet-scd-k=3
+sbatch ${SCRIPT_FIT} droplet  3 em     1000 yes fit-droplet-em-ex-k=3
+sbatch ${SCRIPT_FIT} droplet  3 scd    1000 yes fit-droplet-scd-ex-k=3
+sbatch ${SCRIPT_FIT} droplet  4 em     1000  no fit-droplet-em-k=4
+sbatch ${SCRIPT_FIT} droplet  4 scd    1000  no fit-droplet-scd-k=4
+sbatch ${SCRIPT_FIT} droplet  4 em     1000 yes fit-droplet-em-ex-k=4
+sbatch ${SCRIPT_FIT} droplet  4 scd    1000 yes fit-droplet-scd-ex-k=4
+sbatch ${SCRIPT_FIT} droplet  5 em     1000  no fit-droplet-em-k=5
+sbatch ${SCRIPT_FIT} droplet  5 scd    1000  no fit-droplet-scd-k=5
+sbatch ${SCRIPT_FIT} droplet  5 em     1000 yes fit-droplet-em-ex-k=5
+sbatch ${SCRIPT_FIT} droplet  5 scd    1000 yes fit-droplet-scd-ex-k=5
+
+#                    data     k method    n  ex outfile
+sbatch ${SCRIPT_FIT} droplet  6 em     1000  no fit-droplet-em-k=6
+sbatch ${SCRIPT_FIT} droplet  6 scd    1000  no fit-droplet-scd-k=6
+sbatch ${SCRIPT_FIT} droplet  6 em     1000 yes fit-droplet-em-ex-k=6
+sbatch ${SCRIPT_FIT} droplet  6 scd    1000 yes fit-droplet-scd-ex-k=6
+sbatch ${SCRIPT_FIT} droplet  7 em     1000  no fit-droplet-em-k=7
+sbatch ${SCRIPT_FIT} droplet  7 scd    1000  no fit-droplet-scd-k=7
+sbatch ${SCRIPT_FIT} droplet  7 em     1000 yes fit-droplet-em-ex-k=7
+sbatch ${SCRIPT_FIT} droplet  7 scd    1000 yes fit-droplet-scd-ex-k=7
+sbatch ${SCRIPT_FIT} droplet  8 em     1000  no fit-droplet-em-k=8
+sbatch ${SCRIPT_FIT} droplet  8 scd    1000  no fit-droplet-scd-k=8
+sbatch ${SCRIPT_FIT} droplet  8 em     1000 yes fit-droplet-em-ex-k=8
+sbatch ${SCRIPT_FIT} droplet  8 scd    1000 yes fit-droplet-scd-ex-k=8
+sbatch ${SCRIPT_FIT} droplet  9 em     1000  no fit-droplet-em-k=9
+sbatch ${SCRIPT_FIT} droplet  9 scd    1000  no fit-droplet-scd-k=9
+sbatch ${SCRIPT_FIT} droplet  9 em     1000 yes fit-droplet-em-ex-k=9
+sbatch ${SCRIPT_FIT} droplet  9 scd    1000 yes fit-droplet-scd-ex-k=9
+
+#                    data     k method    n  ex outfile
+sbatch ${SCRIPT_FIT} droplet 10 em     1000  no fit-droplet-em-k=10
+sbatch ${SCRIPT_FIT} droplet 10 scd    1000  no fit-droplet-scd-k=10
+sbatch ${SCRIPT_FIT} droplet 10 em     1000 yes fit-droplet-em-ex-k=10
+sbatch ${SCRIPT_FIT} droplet 10 scd    1000 yes fit-droplet-scd-ex-k=10
+sbatch ${SCRIPT_FIT} droplet 11 em     1000  no fit-droplet-em-k=11
+sbatch ${SCRIPT_FIT} droplet 11 scd    1000  no fit-droplet-scd-k=11
+sbatch ${SCRIPT_FIT} droplet 11 em     1000 yes fit-droplet-em-ex-k=11
+sbatch ${SCRIPT_FIT} droplet 11 scd    1000 yes fit-droplet-scd-ex-k=11
+sbatch ${SCRIPT_FIT} droplet 12 em     1000  no fit-droplet-em-k=12
+sbatch ${SCRIPT_FIT} droplet 12 scd    1000  no fit-droplet-scd-k=12
+sbatch ${SCRIPT_FIT} droplet 12 em     1000 yes fit-droplet-em-ex-k=12
+sbatch ${SCRIPT_FIT} droplet 12 scd    1000 yes fit-droplet-scd-ex-k=12
+
+# Fit factorizations to 68k pbmc data, with and without extrapolation.
+#                    data      k method    n  ex outfile
+sbatch ${SCRIPT_FIT} pbmc_68k  2 em     1000  no fit-pbmc68k-em-k=2
+sbatch ${SCRIPT_FIT} pbmc_68k  2 scd    1000  no fit-pbmc68k-scd-k=2
+sbatch ${SCRIPT_FIT} pbmc_68k  2 em     1000 yes fit-pbmc68k-em-ex-k=2
+sbatch ${SCRIPT_FIT} pbmc_68k  2 scd    1000 yes fit-pbmc68k-scd-ex-k=2
+sbatch ${SCRIPT_FIT} pbmc_68k  3 em     1000  no fit-pbmc68k-em-k=3
+sbatch ${SCRIPT_FIT} pbmc_68k  3 scd    1000  no fit-pbmc68k-scd-k=3
+sbatch ${SCRIPT_FIT} pbmc_68k  3 em     1000 yes fit-pbmc68k-em-ex-k=3
+sbatch ${SCRIPT_FIT} pbmc_68k  3 scd    1000 yes fit-pbmc68k-scd-ex-k=3
+sbatch ${SCRIPT_FIT} pbmc_68k  4 em     1000  no fit-pbmc68k-em-k=4
+sbatch ${SCRIPT_FIT} pbmc_68k  4 scd    1000  no fit-pbmc68k-scd-k=4
+sbatch ${SCRIPT_FIT} pbmc_68k  4 em     1000 yes fit-pbmc68k-em-ex-k=4
+sbatch ${SCRIPT_FIT} pbmc_68k  4 scd    1000 yes fit-pbmc68k-scd-ex-k=4
+sbatch ${SCRIPT_FIT} pbmc_68k  5 em     1000  no fit-pbmc68k-em-k=5
+sbatch ${SCRIPT_FIT} pbmc_68k  5 scd    1000  no fit-pbmc68k-scd-k=5
+sbatch ${SCRIPT_FIT} pbmc_68k  5 em     1000 yes fit-pbmc68k-em-ex-k=5
+sbatch ${SCRIPT_FIT} pbmc_68k  5 scd    1000 yes fit-pbmc68k-scd-ex-k=5
+
+#                    data      k method    n  ex outfile
+sbatch ${SCRIPT_FIT} pbmc_68k  6 em     1000  no fit-pbmc68k-em-k=6
+sbatch ${SCRIPT_FIT} pbmc_68k  6 scd    1000  no fit-pbmc68k-scd-k=6
+sbatch ${SCRIPT_FIT} pbmc_68k  6 em     1000 yes fit-pbmc68k-em-ex-k=6
+sbatch ${SCRIPT_FIT} pbmc_68k  6 scd    1000 yes fit-pbmc68k-scd-ex-k=6
+sbatch ${SCRIPT_FIT} pbmc_68k  7 em     1000  no fit-pbmc68k-em-k=7
+sbatch ${SCRIPT_FIT} pbmc_68k  7 scd    1000  no fit-pbmc68k-scd-k=7
+sbatch ${SCRIPT_FIT} pbmc_68k  7 em     1000 yes fit-pbmc68k-em-ex-k=7
+sbatch ${SCRIPT_FIT} pbmc_68k  7 scd    1000 yes fit-pbmc68k-scd-ex-k=7
+sbatch ${SCRIPT_FIT} pbmc_68k  8 em     1000  no fit-pbmc68k-em-k=8
+sbatch ${SCRIPT_FIT} pbmc_68k  8 scd    1000  no fit-pbmc68k-scd-k=8
+sbatch ${SCRIPT_FIT} pbmc_68k  8 em     1000 yes fit-pbmc68k-em-ex-k=8
+sbatch ${SCRIPT_FIT} pbmc_68k  8 scd    1000 yes fit-pbmc68k-scd-ex-k=8
+sbatch ${SCRIPT_FIT} pbmc_68k  9 em     1000  no fit-pbmc68k-em-k=9
+sbatch ${SCRIPT_FIT} pbmc_68k  9 scd    1000  no fit-pbmc68k-scd-k=9
+sbatch ${SCRIPT_FIT} pbmc_68k  9 em     1000 yes fit-pbmc68k-em-ex-k=9
+sbatch ${SCRIPT_FIT} pbmc_68k  9 scd    1000 yes fit-pbmc68k-scd-ex-k=9
+
+#                    data      k method    n  ex outfile
+sbatch ${SCRIPT_FIT} pbmc_68k 10 em     1000  no fit-pbmc68k-em-k=10
+sbatch ${SCRIPT_FIT} pbmc_68k 10 scd    1000  no fit-pbmc68k-scd-k=10
+sbatch ${SCRIPT_FIT} pbmc_68k 10 em     1000 yes fit-pbmc68k-em-ex-k=10
+sbatch ${SCRIPT_FIT} pbmc_68k 10 scd    1000 yes fit-pbmc68k-scd-ex-k=10
+sbatch ${SCRIPT_FIT} pbmc_68k 11 em     1000  no fit-pbmc68k-em-k=11
+sbatch ${SCRIPT_FIT} pbmc_68k 11 scd    1000  no fit-pbmc68k-scd-k=11
+sbatch ${SCRIPT_FIT} pbmc_68k 11 em     1000 yes fit-pbmc68k-em-ex-k=11
+sbatch ${SCRIPT_FIT} pbmc_68k 11 scd    1000 yes fit-pbmc68k-scd-ex-k=11
+sbatch ${SCRIPT_FIT} pbmc_68k 12 em     1000  no fit-pbmc68k-em-k=12
+sbatch ${SCRIPT_FIT} pbmc_68k 12 scd    1000  no fit-pbmc68k-scd-k=12
+sbatch ${SCRIPT_FIT} pbmc_68k 12 em     1000 yes fit-pbmc68k-em-ex-k=12
+sbatch ${SCRIPT_FIT} pbmc_68k 12 scd    1000 yes fit-pbmc68k-scd-ex-k=12
