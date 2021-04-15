d9dd51aa6acbe32654c41dab65a3c420446844b8
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-04-14 22:37:21 -0500

    Removed temp.R.

--- analysis/temp.R
+++
@@ -1,6 +0,0 @@
-# Top words for topic 1 in newsgroups data, K = 10.
-n   <- colSums(counts)
-lfc <- log10(fit$F[,1]/apply(fit$F[,-1],1,max))
-i   <- which(n > 20)
-print(data.frame(lfc = head(sort(lfc[i],decreasing = TRUE),n = 20)),
-      digits = 2)
