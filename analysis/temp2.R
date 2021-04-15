270f18c55cc66e6d7dd56503c9329a4e4feee9cb
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-04-15 07:39:36 -0500

    Removed temp2.R.

--- analysis/temp2.R
+++
@@ -1,7 +0,0 @@
-# Top genes for topic 6 in 68k PBMC data, K = 7.
-n  <- colSums(counts)
-i  <- which(n > 1000)
-fc <- fit$F[,6]/apply(fit$F[,-6],1,max)
-names(fc) <- genes$symbol
-print(data.frame(fc = head(sort(fc[i],decreasing = TRUE),n = 40)),
-      digits = 2)
