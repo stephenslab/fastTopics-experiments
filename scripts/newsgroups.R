439c6d0d71b042c49de24e0d36792eb0ea22dd51
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-08-12 16:08:01 -0500

    Made some improvements to the newsgroups.R script.

--- scripts/newsgroups.R
+++ scripts/newsgroups.R
@@ -2,8 +2,9 @@ library(Matrix)
 library(fastTopics)
 library(cowplot)
 
-# The number of topics to learn.
+# Script parameters.
 k <- 10
+seed <- 1
 
 # Initialize the sequence of pseudorandom numbers.
 set.seed(1)
@@ -26,18 +27,19 @@ rm(n,rows,cols)
 # updates so that it is difficult for the other algorithms to "escape"
 # this local maximum of the likelihood surface.
 cat("Prefitting Poisson NMF model.\n")
-fit0 <- fit_poisson_nmf(counts,k = k,numiter = 20,method = "em",
+set.seed(seed)
+fit0 <- fit_poisson_nmf(counts,k = k,numiter = 50,method = "em",
+                        init.method = "random",
                         control = list(numiter = 4,nc = 4))
 
-
-# Fit the Poisson NMF model by performing 200 EM updates.
+# Fit the Poisson NMF model by performing 150 EM + 50 SCD updates.
 cat("Running 200 EM updates.\n")
-fit1 <- fit_poisson_nmf(counts,fit0 = fit0,numiter = 200,method = "em",
+fit1 <- fit_poisson_nmf(counts,fit0 = fit0,numiter = 150,method = "em",
                         control = list(numiter = 4,nc = 4))
 
-# Fit the Poisson NMF model by performing 200 extrapolated SCD updates.
+# Fit the Poisson NMF model by performing 150 extrapolated SCD updates.
 cat("Running 200 extrapolated SCD updates.\n")
-fit2 <- fit_poisson_nmf(counts,fit0 = fit0,numiter = 200,method = "scd",
+fit2 <- fit_poisson_nmf(counts,fit0 = fit0,numiter = 150,method = "scd",
                         control = list(numiter = 4,nc = 4,extrapolate = TRUE))
 
 # Plot the improvement in the solution over time.
@@ -45,10 +47,22 @@ p1 <- plot_progress(list(em = fit1,scd = fit2),x = "iter",y = "loglik",
                     add.point.every = 20)
 p2 <- plot_progress(list(em = fit1,scd = fit2),x = "iter",y = "res",
                     add.point.every = 20)
-print(plot_grid(p1,p2))
+dev.new(height = 2,width = 6)
+plot_grid(p1,p2)
 
-# Compare the estimates of the topic proportions.
-plot(poisson2multinom(fit1)$L,poisson2multinom(fit2)$L,pch = 20,
+# Compare the EM and SCD estimates of the topic 
+colors <- c("#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99",
+            "#e31a1c","#fdbf6f","#ff7f00","#cab2d6","#6a3d9a")
+p3 <- structure_plot(fit1,grouping = topics,gap = 20,n = 2000,colors = colors)
+p4 <- structure_plot(fit2,grouping = topics,gap = 20,n = 2000,colors = colors)
+dev.new(height = 3.5,width = 6)
+plot_grid(p3,p4,nrow = 2,ncol = 1)
+
+# Compare the EM and SCD estimates of the topic proportions in a
+# simple scatterplot.
+dev.new(height = 3.5,width = 3)
+plot(poisson2multinom(fit1)$L,poisson2multinom(fit2)$L,pch = 20,cex = 0.5,
      xlab = "EM",ylab = "SCD")
 abline(a = 0,b = 1,col = "magenta",lty = "dotted")
+
 
