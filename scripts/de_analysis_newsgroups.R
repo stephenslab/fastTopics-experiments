a66de8d20c692f79fa5d30677cfbc38523a1fad5
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-01-27 11:51:49 -0600

    Revised the de_analysis_newsgroups.R script.

--- scripts/de_analysis_newsgroups.R
+++ scripts/de_analysis_newsgroups.R
@@ -3,7 +3,7 @@
 # data, with k = 10 topics. These were the steps taken to load R and
 # allocate computing resources for this analysis:
 #
-# sinteractive -p broadwl -c 20 --mem=8G --time=2:00:00
+# sinteractive -p broadwl -c 10 --mem=8G --time=4:00:00
 # module load R/3.5.1
 #
 library(tools)
@@ -21,7 +21,7 @@ fit <- poisson2multinom(fit)
 # Perform the DE analysis.
 t0 <- proc.time()
 de <- de_analysis(fit,counts,pseudocount = 0.1,
-                  control = list(ns = 10000,nc = 20,nsplit = 200))
+                  control = list(ns = 10000,nc = 10,nsplit = 200))
 t1 <- proc.time()
 timing <- t1 - t0
 cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
