b47370031eb69c735053839433c1e0ef9a564f09
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-01-27 10:15:20 -0600

    Made a few improvements to de_analysis_newsgroups.R script.

--- scripts/de_analysis_newsgroups.R
+++ scripts/de_analysis_newsgroups.R
@@ -1,8 +1,11 @@
 # TO DO: Explain here what this script does, and how to use it.
+#
+# sinteractive -p broadwl -c 20 --mem=8G --time=2:00:00
+# module load R/3.5.1
+#
+library(tools)
 library(Matrix)
 library(fastTopics)
-library(ggplot2)
-library(cowplot)
 set.seed(1)
 
 # Load the newsgroups data, and the K = 10 topic model fit.
@@ -15,8 +18,11 @@ fit <- poisson2multinom(fit)
 # Perform the DE analysis.
 t0 <- proc.time()
 de <- de_analysis(fit,counts,pseudocount = 0.1,
-                  control = list(ns = 10000,nc = 20,nsplit = 200))
+                  control = list(ns = 1000,nc = 20,nsplit = 200))
 t1 <- proc.time()
 timing <- t1 - t0
 cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
 
+# Save the results.
+save(list = "de",file = "de-newsgroups.RData")
+resaveRdaFiles("de-newsgroups.RData")
