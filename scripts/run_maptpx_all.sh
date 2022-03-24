c7e713f07df054dabc63a7574d5c7a82cb9995c0
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-03-24 13:10:07 -0500

    Added test runs to run_maptpx_all.sh.

--- scripts/run_maptpx_all.sh
+++ scripts/run_maptpx_all.sh
@@ -2,9 +2,11 @@
 
 # The shell commands below will submit Slurm jobs to run maptpx on all
 # the data sets, for different choices of k (the number of topics),
-# and for different initializations of the model parameters.
+# and for different initializations of the model parameters with
+# fastTopics topic model fits.
 SCRIPT_FIT=run_maptpx.sbatch
 
 # Fit factorizations to the newsgroups data.
-#                    data       init                   outfile                   tmax
-sbatch ${SCRIPT_FIT} newsgroups fit-newsgroups-em-k=10 maptpx-newsgroups-em-k=10 20
+#                    data       init                       outfile                       tmax
+sbatch ${SCRIPT_FIT} newsgroups fit-newsgroups-em-k=10     maptpx-newsgroups-em-k=10     2500
+sbatch ${SCRIPT_FIT} newsgroups fit-newsgroups-scd-ex-k=10 maptpx-newsgroups-scd-ex-k=10 2500
