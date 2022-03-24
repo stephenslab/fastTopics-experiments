4c51c8a4e54043937c9599ed50deedd3b874284a
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-03-24 08:24:05 -0500

    I have an initial draft of the new run_maptpx.R script.

---
+++ scripts/run_maptpx.R
@@ -0,0 +1,54 @@
+#! /usr/bin/env Rscript
+#
+# TO DO: Add comments here explaining what this script does, and how
+# to use it.
+#
+# NOTES:
+# - For newsgroups, request 8 GB and 4 CPUs.
+#
+countsfile <- "../data/newsgroups.RData"
+pnmf_file  <- "../output/newsgroups/rds/fit-newsgroups-em-k=10.rds"
+outfile    <- "maptpx-newsgroups-em-k=10.rds"
+tmax       <- 2500
+
+# Load a few packages.
+library(optparse)
+library(Matrix)
+library(maptpx)
+library(fastTopics)
+
+# Initialize the sequence of pseudorandom numbers.
+set.seed(1)
+
+# LOAD DATA
+# ---------
+# Load the previously prepared count data.
+cat(sprintf("Loading data from %s.\n",countsfile))
+load(countsfile)
+cat(sprintf("Loaded %d x %d counts matrix.\n",nrow(counts),ncol(counts)))
+
+# LOAD POISSON NMF FIT
+# --------------------
+# Load the Poisson NMF model previously fitted using fastTopics.
+cat(sprintf("Loading Poisson NMF model from %s\n",pnmf_file))
+fit0 <- readRDS(pnmf_file)$fit
+k <- ncol(fit0$F)
+
+# RUN MAPTPX
+# ----------
+# Now we are ready to perform MAP estimation of F, L using maptpx.
+t0 <- proc.time()
+maptpx <- topics(counts,k,shape = 1,initopics = fit0$F,omega = fit0$L,
+                 tol = 1e-15,tmax = tmax,ord = FALSE,verb = 1)
+t1 <- proc.time()
+timing <- t1 - t0
+cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
+
+# SAVE RESULTS
+# ------------
+cat("Saving results.\n")
+saveRDS(maptpx,file = outfile)
+
+# SESSION INFO
+# ------------
+print(sessionInfo())
