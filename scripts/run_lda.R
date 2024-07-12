03706d94ab65c5950c2488726e88cdc9f94faf55
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-07-12 11:17:01 -0500

    A few fixes to the LDA scripts.

--- scripts/run_lda.R
+++ scripts/run_lda.R
@@ -4,34 +4,21 @@
 #
 # This script is intended to be run from the command-line shell, with
 # options that are processed with the optparse package. For example,
-# to fit a Poisson non-negative matrix factorization to counts data
-# test.RData by running 500 SCD updates with extrapolation, 2 threads,
-# with estimates initialized from test_prefit.rds, and with results
-# saved to test_fit.rds, run this command:
+# to run LDA on counts data test.RData, with estimates initialized
+# from test.rds, run this
+# command:
 #
-#   ./fit_poisson_nmf.R --counts test.RData --prefit test_prefit.rds \
-#     --method scd -n 500 --extrapolate --nc 2 -o test_fit.rds
-#
-# Running the script without specifying any options will fit a Poisson
-# non-negative matrix factorization by running 1,000 EM updates
-# without extrapolation, without multithreading, initialized to
-# prefit.rds, and with results saved to out.rds.
-#
-# The input .RData file specified by --counts should contain a matrix,
-# "counts", containing the count data that will be provided as input
-# to fit_poisson_nmf. The input .rds file specified by --prefit should
-# contain an object "fit", an output from a previous call to
-# fit_poisson_nmf or init_poisson_nmf.
+#   ./run_lda.R --counts test.RData --init test.rds
 #
 
-#
-# sinteractive -c 4 --mem=8G --time=24:00:00 (for 68k PBMC data set)
-# sinteractive -c 4 --mem=32G --time=24:00:00 (for other data sets)
-# module load R/3.5.1
-# R
-# > .libPaths()[1]
-# [1] "/home/pcarbo/R_libs"
-#
+# countsfile <- "../data/newsgroups.RData"
+# countsfile <- "../data/nips.RData"
+# countsfile <- "../data/droplet.RData"
+# countsfile <- "../data/pbmc_68k.RData"
+# initfile   <- "../output/newsgroups/rds/fit-newsgroups-scd-ex-k=10.rds"
+# initfile   <- "../output/nips/rds/fit-nips-scd-ex-k=10.rds"
+# initfile   <- "../output/droplet/rds/fit-droplet-scd-ex-k=10.rds"
+# initfile   <- "../output/pbmc68k/rds/fit-pbmc68k-scd-ex-k=10.rds"
 
 # Load a few packages.
 library(optparse)
@@ -42,7 +29,7 @@ library(topicmodels)
 source("../code/smallsim_functions.R")
 
 # Process the command-line arguments.
-# parser <- OptionParser()
+parser <- OptionParser()
 parser <- add_option(parser,"--counts",type="character",default="counts.RData")
 parser <- add_option(parser,"--init",type = "character",default="init.rds")
 out    <- parse_args(parser)
@@ -54,15 +41,6 @@ rm(parser,out)
 
 # Initialize the sequence of pseudorandom numbers.
 set.seed(1)
-
-# countsfile <- "../data/newsgroups.RData"
-# countsfile <- "../data/nips.RData"
-# countsfile <- "../data/droplet.RData"
-# countsfile <- "../data/pbmc_68k.RData"
-# initfile   <- "../output/newsgroups/rds/fit-newsgroups-scd-ex-k=10.rds"
-# initfile   <- "../output/nips/rds/fit-nips-scd-ex-k=10.rds"
-# initfile   <- "../output/droplet/rds/fit-droplet-scd-ex-k=10.rds"
-# initfile   <- "../output/pbmc68k/rds/fit-pbmc68k-scd-ex-k=10.rds"
 
 # LOAD DATA
 # ---------
--- scripts/run_lda_all.sh
+++ scripts/run_lda_all.sh
@@ -2,9 +2,9 @@
 
 # The shell commands below will submit Slurm jobs to run LDA for all
 # data sets, and for different choices of K and initialization.
-SCRIPT=run_lda.sbatch
+MAIN_SCRIPT=run_lda.sbatch
 
 # Run LDA on newsgroups data.
 #
-#                data       initfile
-sbatch ${SCRIPT} newsgroups newsgroups/rds/fit-newsgroups-scd-ex-k=10
+#                     data       initfile
+sbatch ${MAIN_SCRIPT} newsgroups newsgroups/rds/fit-newsgroups-scd-ex-k=10
