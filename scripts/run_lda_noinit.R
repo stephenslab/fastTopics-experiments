62dd72ab0e35454647d7943df105e25d5a3f2557
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-08-19 11:59:38 -0500

    I have a first draft of the 'lda_noinit' scripts.

--- scripts/run_lda_noinit.R
+++ scripts/run_lda_noinit.R
@@ -4,30 +4,31 @@
 #
 # This script is intended to be run from the command-line shell, with
 # options that are processed with the optparse package. For example,
-# to run LDA on counts data test.RData, with estimates initialized
-# from test.rds, run this
-# command:
+# to run LDA on counts data test.RData, with k = 4, run this command:
 #
-#   ./run_lda.R --counts test.RData --init test.rds
+#   ./run_lda_noinit.R --counts test.RData --k 4
 #
 
+# countsfile <- "../data/newsgroups.RData"
+# outfile <- "lda-newsgroups-k=10.rds"
+# k <- 10
+
 # Load a few packages.
-library(optparse)
 library(Matrix)
-library(fastTopics)
+library(optparse)
 library(tm)
 library(topicmodels)
-source("../code/smallsim_functions.R")
 
 # Process the command-line arguments.
 parser <- OptionParser()
 parser <- add_option(parser,"--counts",type="character",default="counts.RData")
-parser <- add_option(parser,"--init",type = "character",default="init.rds")
+parser <- add_option(parser,"--k",type = "integer",default = 3)
 out    <- parse_args(parser)
 countsfile <- out$counts
-initfile   <- out$init
-outfile    <- tail(unlist(strsplit(initfile,"/")),n = 1)
-outfile    <- paste0("lda",substr(outfile,4,100))
+k          <- out$k
+outfile    <- tail(unlist(strsplit(countsfile,"/")),n = 1)
+outfile    <- substr(outfile,1,nchar(outfile) - 6)
+outfile    <- paste0("lda-",outfile,"-k=",k,".rds")
 rm(parser,out)
 
 # Initialize the sequence of pseudorandom numbers.
@@ -40,23 +41,18 @@ cat(sprintf("Loading data from %s.\n",countsfile))
 load(countsfile)
 cat(sprintf("Loaded %d x %d counts matrix.\n",nrow(counts),ncol(counts)))
 
-# LOAD POISSON NMF FIT
-# --------------------
-# Load the Poisson NMF model previously fitted using fastTopics.
-cat(sprintf("Loading Poisson NMF model from %s\n",initfile))
-fit0 <- readRDS(initfile)$fit
-fit0 <- poisson2multinom(fit0)
-k    <- ncol(fit0$F)
-
 # RUN LDA
 # -------
 # Now we are ready to perform variational inference for the LDA model.
-#
-# For the 68k PBMC data with k = 10, this step took roughly 20 min
-# per iteration.
-#
+counts <- as.DocumentTermMatrix(counts,weighting = c("term frequency","tf"))
 t0 <- proc.time()
-lda <- run_lda(counts,fit0,numiter = 100,estimate.alpha = FALSE,verbose = 1)
+lda <- LDA(counts,k,
+           control = list(alpha = 1,
+                          estimate.alpha = FALSE,
+                          verbose = 1,
+                          em = list(iter.max = 10,tol = 0),
+                          var = list(iter.max = 20,tol = 0),
+                          keep = 1))
 t1 <- proc.time()
 timing <- t1 - t0
 cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
