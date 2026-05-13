2422aeff2481d8e8b73cc204fdc477c8ef14d5ba
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-05-13 09:19:23 -0500

    Started new analysis script omim_topics.R.

---
+++ analysis/omim_topics.R
@@ -0,0 +1,14 @@
+library(fastTopics)
+load("../data/omim.RData")
+set.seed(1)
+k <- 40
+tm <- fit_poisson_nmf(counts,k = k,init.method = "random",method = "em",
+                      numiter = 20,verbose = "detailed",
+                      control = list(numiter = 4,nc = 8,extrapolate = FALSE))
+tm <- fit_poisson_nmf(counts,fit0 = tm,method = "scd",numiter = 100,
+                      control = list(numiter = 4,nc = 8,extrapolate = FALSE),
+                      verbose = "detailed")
+tm <- fit_poisson_nmf(counts,fit0 = tm,method = "scd",numiter = 100,
+                      control = list(numiter = 4,nc = 8,extrapolate = TRUE),
+                      verbose = "detailed")
+
