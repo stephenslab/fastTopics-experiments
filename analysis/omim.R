dc29f2d3e9afd8af07e8e8bab303fa5a5d80db86
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-07-21 12:34:19 -0500

    Small edit to exploratory analysis script omim.R.

--- analysis/omim.R
+++ analysis/omim.R
@@ -1,3 +1,31 @@
+library(Matrix)
+library(rsvd)
+library(uwot)
+library(fastTopics)
+library(ggplot2)
+library(cowplot)
+load("../data/omim.RData")
+load("../output/omim_topics_k=40.RData")
+L <- poisson2multinom(tm)$L
+F <- poisson2multinom(tm)$F
+set.seed(1)
+a <- 1
+s <- rowSums(counts)
+s <- s/mean(s)
+shifted_log_counts <- log1p(counts/(a*s))
+U <- rsvd(counts,k = 100)$u
+Y <- umap(U,n_neighbors = 40,metric = "cosine",
+          min_dist = 0.1,n_threads = 4,
+          verbose = TRUE)
+pdat <- cbind(data.frame(umap1 = Y[,1],umap2 = Y[,2]),L)
+# k8, k10, k32
+p1 <- ggplot(pdat,aes(x = umap1,y = umap2,color = k8)) +
+  geom_point() +
+  scale_color_gradient(low = "gainsboro",high = "black") +
+  theme_cowplot(font_size = 12)
+
+stop()
+
 # Inspecting the topics:
 k <- 1
 L <- poisson2multinom(tm)$L
