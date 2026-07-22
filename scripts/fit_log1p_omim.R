2e0adc9df30a1117c304c66d99239b7a3902218f
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-07-22 13:56:28 -0500

    I have a first draft of the fit_log1p_omim.R script.

---
+++ scripts/fit_log1p_omim.R
@@ -0,0 +1,15 @@
+library(tools)
+library(Matrix)
+library(log1pNMF)
+load("../data/omim.RData")
+set.seed(1)
+k <- 10
+s <- Matrix::rowSums(counts)
+s <- s/mean(s)
+fit_log1p <- fit_poisson_log1p_nmf(counts,k,s,cc = 1,loglik = "approx",
+                                   init_method = "rank1",
+                                   control = list(maxiter = 20,
+                                                  verbose = TRUE))
+# Save the model fit to an .Rdata file.
+save(list = "fit_log1p",file = "omim_log1p_k=40.RData")
+# resaveRdaFiles("omim_log1p_k=40.RData")
